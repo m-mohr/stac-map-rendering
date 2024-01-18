0a24c2f83cf737a6cc3c4a77c2b60fd0166fc923
silvester-pari <silvester.pari@eox.at>
2024-01-18 17:36:40 +0100

    updates

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const eoxMap = document.querySelector("eox-map");
 eoxMap.layers = [
   {
     type: "STAC",
-    displayWebMapLink: true,
-    url: "./example/NO2/collection.json",
+    displayPreview: true,
+    url: "https://tamn.snapplanet.io/search?bbox=125.727770,-29.514858,133.412707,-23.673395&collections=S2",
     properties: {
       id: "stac",
       title: "STAC",
@@ -29,22 +29,22 @@ eoxMap.layers = [
 
 let stacInfo;
 
-eoxMap.map.once("loadend", () => {
-  const stacGroup = eoxMap.map.getLayers().getArray()[1];
-  const wmsLayer = stacGroup.getLayers().getArray()[1];
-  stacGroup.setVisible(false);
-  stacInfo = wmsLayer.get("stac");
-  wmsLayer.set("id", stacInfo.title);
-  wmsLayer.set("title", stacInfo.title);
-  stacGroup.getLayers().remove(wmsLayer);
-  eoxMap.map.addLayer(wmsLayer);
-});
+// eoxMap.map.once("loadend", () => {
+//   const stacGroup = eoxMap.map.getLayers().getArray()[1];
+//   const wmsLayer = stacGroup.getLayers().getArray()[1];
+//   stacGroup.setVisible(false);
+//   stacInfo = wmsLayer.get("stac");
+//   wmsLayer.set("id", stacInfo.title);
+//   wmsLayer.set("title", stacInfo.title);
+//   stacGroup.getLayers().remove(wmsLayer);
+//   eoxMap.map.addLayer(wmsLayer);
+// });
 
-const eoxTimeControl = document.createElement("eox-timecontrol");
-eoxTimeControl.for = "eox-map";
-eoxTimeControl.layer = "NO2";
+// const eoxTimeControl = document.createElement("eox-timecontrol");
+// eoxTimeControl.for = "eox-map";
+// eoxTimeControl.layer = "NO2";
 
-eoxTimeControl.animationProperty = "TIME";
-eoxTimeControl.animationValues = ["2023-10-30", "2023-11-06", "2023-11-13"];
+// eoxTimeControl.animationProperty = "TIME";
+// eoxTimeControl.animationValues = ["2023-10-30", "2023-11-06", "2023-11-13"];
 
-document.body.appendChild(eoxTimeControl);
+// document.body.appendChild(eoxTimeControl);
